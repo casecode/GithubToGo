13e35ef3a6913d03c7a753acacbb1eb78e5fad69
Casey White <casey.r.white@gmail.com>
2014-10-20 13:48:21 -0700

    Embed SplitVC in SplitContainerVC

---
+++ GithubToGo/SplitContainerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplitContainerViewController.swift
+//  GithubToGo
+//
+//  Created by Casey R White on 10/20/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+import UIKit
+
+class SplitContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
