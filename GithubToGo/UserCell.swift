d5bb3047511040953c115cba97d559134e2102b8
Casey White <casey.r.white@gmail.com>
2014-10-25 21:00:49 -0700

    Add UsersVC

---
+++ GithubToGo/UserCell.swift
@@ -0,0 +1,16 @@
+//
+//  UserCell.swift
+//  GithubToGo
+//
+//  Created by Casey R White on 10/25/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userAvatarImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+}
---
+++ GithubToGo/UsersViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UsersViewController.swift
+//  GithubToGo
+//
+//  Created by Casey R White on 10/25/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let ghService = GithubService.sharedInstance
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.ghService.sessionAuthenticated() {
+            println("Session authenticated")
+        } else {
+            println("Requesting oauth access")
+            // Add alert
+            self.ghService.requestOAuthAccess()
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier("USER_CELL", forIndexPath: indexPath) as UserCell
+        return cell
+    }
+
+}
